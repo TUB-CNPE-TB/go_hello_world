406a5dd6b7d2a15260bf223a1a76bc3de7f34304
ciphersmaug <39407479+ciphersmaug@users.noreply.github.com>
2021-11-29 11:43:29 +0100

    applied suggested changes and changed port to port 80

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-    var jsonData = []byte(`{
+	var jsonData = []byte(`{
         "message": "Hello World!"
     }`)
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        w.Header().Set("Content-Type", "application/json")
-        w.Write(jsonData)
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonData)
+	})
 
-    fmt.Printf("Starting server at port 9090\n")
-    if err := http.ListenAndServe(":9090", nil); err != nil {
-        log.Fatal(err)
-    }
+	fmt.Printf("Starting server at port 80\n")
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		log.Fatal(err)
+	}
 }
